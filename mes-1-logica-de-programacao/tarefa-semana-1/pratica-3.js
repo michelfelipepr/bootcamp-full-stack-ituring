4783c346ea081414f461f3f2f2bf06e2d7d90fd3
Michel Felipe Prasnievski <michelfelipepr@hotmail.com>
2022-10-11 17:34:46 -0300

    Atividades da pratica3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,45 @@
+const nome = "Michel"
+const email = "michelfelipepr@hotmail.com"
+const profissao = "Supervisor de Operação em TI"
+const idade = 35
+const conhecer = "Fernando de Noronha"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(conhecer)
+
+//console.log("Na linha abaixo o tipo de variavel")
+// Identificar Tipo de Variavel
+//const tipoNome = typeof(nome)
+//const tipoEmail = typeof(email)
+//const tipoProfissao = typeof(profissao)
+//const tipoIdade = typeof(idade)
+//const tipoConhecer = typeof(conhecer)
+//console.log(tipoNome)
+//console.log(tipoEmail)
+//console.log(tipoProfissao)
+//console.log(tipoIdade)
+//console.log(tipoConhecer)
+
+console.log(" * Agora, na linha abaixo, se é STRING será verdadeira, do contrario sera falsa *")
+// VERDADEIRO OU FALSO
+const tipoNome = typeof(nome) == "string"
+const tipoEmail = typeof(email) == "string"
+const tipoProfissao = typeof(profissao) == "string"
+const tipoIdade = typeof(idade) == "string"
+const tipoConhecer = typeof(conhecer) == "string"
+
+console.log(tipoNome)
+console.log(tipoEmail)
+console.log(tipoProfissao)
+console.log(tipoIdade)
+console.log(tipoConhecer)
+
+
+// retorno true se valor for do tipo number
+//const valor = 1
+//const ehNumero = typeof(valor) == "number"
+
+//console.log (ehNumero)
