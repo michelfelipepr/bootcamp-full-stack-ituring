dd08bca17cccaf1b8eff96e25c8b7f5f540e9de6
Michel Felipe Prasnievski <michelfelipepr@hotmail.com>
2022-10-11 17:07:49 -0300

    Arquivo pratica1

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,3 +3,22 @@ const email = "michelfelipepr@hotmail.com"
 const profissao = "Supervisor de Operação em TI"
 const idade = 35
 const conhecer = "Fernando de Noronha"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(conhecer)
+
+// Identificar Tipo de Variavel
+const tipoNome = typeof(nome)
+const tipoEmail = typeof(email)
+const tipoProfissao = typeof(profissao)
+const tipoIdade = typeof(idade)
+const tipoConhecer = typeof(conhecer)
+
+console.log(tipoNome)
+console.log(tipoEmail)
+console.log(tipoProfissao)
+console.log(tipoIdade)
+console.log(tipoConhecer)
