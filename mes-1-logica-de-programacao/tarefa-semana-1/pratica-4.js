fc866f79d480583cb5b7703d99c32ca9a28c6ee8
Michel Felipe Prasnievski <michelfelipepr@hotmail.com>
2022-10-11 18:21:22 -0300

    atividades restantes da pratica 4 a pratica 6

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-4.js
@@ -0,0 +1,7 @@
+const distancia = 238900
+const kilometrospadrao = 0.62137
+
+const conversao = (distancia / kilometrospadrao)
+
+console.log('Tamanho em KM da distancia é ' + conversao)
+
---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,12 @@
+const nome = "Michel"
+const email = "michelfelipepr@hotmail.com"
+const profissao = "Supervisor de Operação em TI"
+const idade = 35
+const conhecer = "Fernando de Noronha"
+
+console.log('Olá,' + nome)
+console.log('Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ' +conhecer)
+console.log('Confirma pra gente alguns dados? A sua idade é ' +idade, 'você é' +profissao, 'e o seu email é' +email, 'Ficamos no aguardo. Muito obrigado e boa viagem!')
+
+
+
