b93e348e18e772f470de8fa1f143ce4ff3854221
Michel Felipe Prasnievski <michelfelipepr@hotmail.com>
2022-10-17 10:11:25 -0300

    pratica 2 funcional

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,19 @@
+function NomeDoProduto(smartphone){
+    console.log("Fizemos a entrega do produto " + smartphone)
+}
+
+NomeDoProduto("SAMSUNG")
+
+function NomeDaPessoaQueRecebeuOProduto(recebimento){
+    console.log("O seu produto foi recebido por: " + recebimento)
+    console.log("Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.")
+}
+
+NomeDaPessoaQueRecebeuOProduto("Michel")
+
+function NomeDaPessoaQueEnviouOEmail(enivouemail){
+    console.log("Atenciosamente, " + enivouemail)
+}
+
+NomeDaPessoaQueEnviouOEmail("Loja SAMSUNG")
+
