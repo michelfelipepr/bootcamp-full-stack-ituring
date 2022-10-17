a3ff54d552557e97c7051f80b44a8a07d05bf06b
Michel Felipe Prasnievski <michelfelipepr@hotmail.com>
2022-10-17 14:11:58 -0300

    pratica 5

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-5.js
@@ -0,0 +1,10 @@
+
+const calculaDesconto = function  (valorProduto, porcentagemDesconto) {
+    const valorFinal = valorProduto - valorProduto*porcentagemDesconto/100
+    return valorFinal
+    }
+    
+    const valorComDesconto = calculaDesconto(100, 5)
+    
+    console.log(valorComDesconto)
+    
